7ad43444974f1f5928bb9f22f2d02e48fcb01c25
Admin <Admin@DESKTOP-9833F0D>
2022-04-03 21:08:04 +0530

    1st commit march29

---
+++ statics/src/statics/Statics1.java
@@ -0,0 +1,30 @@
+package statics;
+
+import java.util.Scanner;
+
+public class Statics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number of rows");
+		Scanner Scan = new Scanner (System.in);
+		int n = Scan.nextInt();
+		for(int i=0;i<=n-1;i++)
+		{
+			for(int j=0;j<=n-1;j++)
+			{
+				if(i==0||j==0||j==n-1||i==n-1)
+				{
+					System.out.println("*");
+				}
+				else
+				{
+					System.out.println(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
+
